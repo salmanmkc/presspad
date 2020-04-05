9f2137faa90bdf7b68de14140406cd9fcc2f8d35
ramy <ramyshurafa@hotmail.com>
2020-04-05 17:53:56 +0300

    create side menue placeholder

---
+++ client/src/components/Common/SideMenu/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+import { Wrapper } from './style';
+
+const SideMenu = () => <Wrapper />;
+
+export default SideMenu;
---
+++ client/src/components/Common/SideMenu/style.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+// eslint-disable-next-line import/prefer-default-export
+export const Wrapper = styled.div`
+  width: 245px;
+  min-height: 100vh;
+  background-color: blue;
+`;
