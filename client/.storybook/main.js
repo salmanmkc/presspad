89af69fc65e8d55a5f06916ad696c2f914b903fe
ramy <ramyshurafa@hotmail.com>
2020-04-07 13:09:05 +0300

    antd theming with storybook

--- client/.storybook/main.js
+++ client/.storybook/main.js
@@ -1,3 +1,10 @@
+const path = require("path")
+const {
+  override,
+  fixBabelImports,
+  addLessLoader,
+} = require("customize-cra");
+
 module.exports = {
   stories: ['../src/**/*.stories.js'],
   addons: [
@@ -5,4 +12,19 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
   ],
+
+  webpack: override(
+    fixBabelImports('import', {
+      libraryName: 'antd',
+      libraryDirectory: 'es',
+      style: true,
+    }),
+    addLessLoader({
+      javascriptEnabled: true,
+      modifyVars: {
+        '@primary-color': '#2F2F2F', // primary color for all components
+
+      },
+    }),
+  )
 };
