ed800aef9f29d194499044f98165ce5275d2237c
Ahmed Shatat <a.shatat@hotmail.com>
2020-04-09 16:27:05 +0300

    create TipsCard component
    #534

---
+++ client/src/components/Pages/BookingView/TipsCard/TipsCard.style.js
@@ -0,0 +1,34 @@
+import styled from 'styled-components';
+
+export const Wrapper = styled.div`
+  max-width: 410px;
+  height: 219px;
+  margin: 0 auto;
+  padding: ${({ theme: { spacings } }) => `${spacings[4]} ${spacings[4]}`};
+  box-shadow: ${({ theme }) => theme.shadows.card};
+  @media ${({ theme: { breakpoints } }) => breakpoints.tablet} {
+    padding: ${({ theme: { spacings } }) => `${spacings[4]} ${spacings[5]}`};
+  }
+`;
+
+export const TitleWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-bottom: ${({ theme: { spacings } }) => spacings[4]};
+`;
+
+export const ItemLi = styled.li`
+  display: flex;
+  align-items: center;
+
+  ::before {
+    content: '';
+    display: inline-block;
+    margin-right: ${({ theme: { spacings } }) => spacings[4]};
+    width: 16px;
+    height: 16px;
+    border: 1px solid ${({ theme }) => theme.colors.darkerGray};
+    border-radius: 50%;
+  }
+`;
