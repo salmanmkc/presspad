0c8dadd452bdd819af6c9643a7a8f04c795a6ad2
Ahmed Shatat <a.shatat@hotmail.com>
2020-04-09 21:24:49 +0300

    add AbsoluteWrapper to the BookingDate component
    #534

--- client/src/components/Pages/BookingView/BookingDates/BookingDates.style.js
+++ client/src/components/Pages/BookingView/BookingDates/BookingDates.style.js
@@ -1,21 +1,31 @@
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
+  height: calc(182px - ${({ theme }) => theme.spacings.sideMenuLayout.bottom});
+  margin-top: ${({ theme }) => theme.spacings[6]};
+  @media ${({ theme: { breakpoints } }) => breakpoints.tablet} {
+    height: 0;
+    margin-top: 0;
+  }
+`;
+export const AbsoluteWrapper = styled.div`
   background-color: ${({ theme }) => theme.colors.pink};
   color: ${({ theme }) => theme.colors.white};
   font-family: Glacial Indifference;
   font-style: normal;
   font-weight: bold;
   width: 100%;
+  min-height: 182px;
   position: absolute;
-  right: 0px;
+  right: 0;
+  bottom: 0;
   padding-top: ${({ theme: { spacings } }) => spacings[3]};
-  padding-bottom: ${({ theme: { spacings } }) => spacings[4]};
   padding-left: ${({ theme: { spacings } }) => spacings[7]};
   @media ${({ theme: { breakpoints } }) => breakpoints.tablet} {
     padding-bottom: ${({ theme: { spacings } }) => spacings[5]};
     padding-top: ${({ theme: { spacings } }) => spacings[5]};
-    width: 507px;
+    max-width: 507px;
+    width: 50%;
     height: 287px;
     top: 162px;
     > h6:first-child {
--- client/src/components/Pages/BookingView/BookingDates/index.js
+++ client/src/components/Pages/BookingView/BookingDates/index.js
@@ -2,7 +2,12 @@ import React from 'react';
 import moment from 'moment';
 
 import { H4, H7C } from '../../../Common/Typography';
-import { Wrapper, BookingDateWrapper, DayNumber } from './BookingDates.style';
+import {
+  Wrapper,
+  AbsoluteWrapper,
+  BookingDateWrapper,
+  DayNumber,
+} from './BookingDates.style';
 
 const BookingInfo = ({ startDate, endDate, price, intern }) => {
   const startDay = moment(startDate).format('DD');
@@ -12,24 +17,26 @@ const BookingInfo = ({ startDate, endDate, price, intern }) => {
 
   return (
     <Wrapper>
-      <H7C color="white">BOOKING DATES</H7C>
-      <BookingDateWrapper>
-        <div>
-          <DayNumber>{startDay}</DayNumber>
-          <H7C color="white">{startMonth}</H7C>
-        </div>
-        <DayNumber>-</DayNumber>
-        <div>
-          <DayNumber>{endDay}</DayNumber>
-          <H7C color="white">{endMonth}</H7C>
-        </div>
-      </BookingDateWrapper>
-      {intern ? (
-        <H7C color="white">full price for the period</H7C>
-      ) : (
-        <H7C color="white">earnings</H7C>
-      )}
-      <H4 color="white">£{price}</H4>
+      <AbsoluteWrapper>
+        <H7C color="white">BOOKING DATES</H7C>
+        <BookingDateWrapper>
+          <div>
+            <DayNumber>{startDay}</DayNumber>
+            <H7C color="white">{startMonth}</H7C>
+          </div>
+          <DayNumber>-</DayNumber>
+          <div>
+            <DayNumber>{endDay}</DayNumber>
+            <H7C color="white">{endMonth}</H7C>
+          </div>
+        </BookingDateWrapper>
+        {intern ? (
+          <H7C color="white">full price for the period</H7C>
+        ) : (
+          <H7C color="white">earnings</H7C>
+        )}
+        <H4 color="white">£{price}</H4>
+      </AbsoluteWrapper>
     </Wrapper>
   );
 };
