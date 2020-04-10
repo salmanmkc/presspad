d8579b966c399fc7c2a957f44752bd49dfff37da
ramy <ramyshurafa@hotmail.com>
2020-04-10 19:40:44 +0300

    rename storybook file

--- client/src/stories/6-HostCard.stories.js
+++ client/src/stories/6-HostCard.stories.js
@@ -1,15 +1,10 @@
 import React from 'react';
-import { action } from '@storybook/addon-actions';
-import Form from '../components/Pages/SearchHosts/Form';
-import Hero from '../components/Pages/SearchHosts/Hero';
 import HostCard from '../components/Pages/SearchHosts/HostCard';
 
 export default {
   title: 'SearchForm',
 };
 
-export const Default = () => <Form onClick={action('button-click')} />;
-export const HeroComponent = () => <Hero onClick={action('button-click')} />;
 export const HostCardshort = () => (
   <HostCard
     city="liverpool"
