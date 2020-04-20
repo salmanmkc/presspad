87951d74bfe8a84d735047e1a5ba084b71f2e223
Simon Duprée <dupreenator@gmail.com>
2020-04-20 14:54:31 +0200

    570 show bookingdetails internship (#586)
    * setup common component
    
    * added styles
    
    * added booking details comp to update page
    
    * update styling
    
    * created common component booking details box
    
    * added common component scroll to top and updated price calc for update internshio

---
+++ client/src/components/Common/ScrollToTop/index.js
@@ -0,0 +1,30 @@
+import { useEffect } from 'react';
+import { useHistory } from 'react-router-dom';
+
+// Component that attaches scroll to top hanler on router change
+// renders nothing, just attaches side effects
+const ScrollToTopControlller = () => {
+  const history = useHistory();
+
+  const {
+    location: { pathname, search },
+  } = history;
+
+  useEffect(() => {
+    try {
+      window.scroll({
+        top: 0,
+        left: 0,
+        behavior: 'smooth',
+      });
+    } catch (error) {
+      // fallback for older browsers
+      window.scrollTo(0, 0);
+    }
+  }, [pathname, search]);
+
+  // renders nothing, since nothing is needed
+  return null;
+};
+
+export default ScrollToTopControlller;
