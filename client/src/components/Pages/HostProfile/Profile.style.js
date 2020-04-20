293d380a028fb22379539103a1c9180923cc4bd6
Simon Dupree <dupreenator@gmail.com>
2020-04-20 19:07:58 +0200

    removed content wrapper

--- client/src/components/Pages/HostProfile/Profile.style.js
+++ client/src/components/Pages/HostProfile/Profile.style.js
@@ -9,35 +9,6 @@ const classNames = {
   reactCalendarNavigation: '.react-calendar__navigation',
 };
 
-export const ContentWrapper = styled.div`
-  position: relative;
-  display: flex;
-  flex-direction: column;
-  width: 100%;
-  padding: ${({ theme }) =>
-    `${theme.spacings.sideMenuLayout.top} 3% ${theme.spacings.sideMenuLayout.bottom}`};
-
-  @media ${breakpoints.mobileM} {
-    padding: ${({ theme }) =>
-      `${theme.spacings.sideMenuLayout.top} 7% ${theme.spacings.sideMenuLayout.bottom}`};
-  }
-
-  @media ${breakpoints.tablet} {
-    padding: ${({ theme }) =>
-      `${theme.spacings.sideMenuLayout.top} 4% ${theme.spacings.sideMenuLayout.bottom}`};
-  }
-
-  @media ${breakpoints.laptop} {
-    padding: ${({ theme }) =>
-      `${theme.spacings.sideMenuLayout.top} 5% ${theme.spacings.sideMenuLayout.bottom}`};
-  }
-
-  @media ${breakpoints.laptopL} {
-    padding: ${({ theme }) =>
-      `${theme.spacings.sideMenuLayout.top} 7% ${theme.spacings.sideMenuLayout.bottom}`}; /** 7% equal 100px on 1440px screens */
-  }
-`;
-
 // used to split page into 2 main parts
 export const PageDivider = styled.section`
   display: flex;
