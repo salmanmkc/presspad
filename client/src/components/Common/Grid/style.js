636368b5caf6d32a3a24566e4f51459c6d1bd109
ramy <ramyshurafa@hotmail.com>
2020-07-28 12:51:39 +0300

    fix media queries

--- client/src/components/Common/Grid/style.js
+++ client/src/components/Common/Grid/style.js
@@ -8,17 +8,18 @@ export const Row = styled.div`
   margin-right: -${({ inner, theme }) => (inner ? theme.vars.gridGutter / 2 : 0)}px !important;
   width: ${({ inner, theme }) =>
     inner ? `calc(100% + ${theme.vars.gridGutter}px)` : '100%'};
-  justify-content: ${({ jc }) => jc || 'flex-start'};
   margin-top: ${({ theme, mt }) => theme.spacings[mt] || 0};
   margin-bottom: ${({ theme, mb }) => theme.spacings[mb] || 0};
   align-items: ${({ ai }) => ai || 'flex-start'};
 
-  @media ${breakpoints.TABLET_WIDTH} {
+  justify-content: ${({ jcT, jc, jcM }) => jcM || jcT || jc || 'flex-start'};
+
+  @media ${breakpoints.mobileM} {
     justify-content: ${({ jcT, jc }) => jcT || jc || 'flex-start'};
   }
 
-  @media ${breakpoints.mobileM} {
-    justify-content: ${({ jcT, jc, jcM }) => jcM || jcT || jc || 'flex-start'};
+  @media ${breakpoints.tablet} {
+    justify-content: ${({ jc }) => jc || 'flex-start'};
   }
 `;
 
@@ -31,13 +32,14 @@ export const Col = styled.div`
   width: 100%;
   min-height: 1px;
   height: 100%;
-  flex-basis: ${({ c3, theme }) =>
-    `calc(${(c3 / theme.vars.columns.desktop) * 100}%)`};
-  max-width: ${({ c3, theme }) =>
-    `calc(${(c3 / theme.vars.columns.desktop) * 100}%)`};
-  display: ${({ c3 }) => (c3 ? 'block' : 'none')};
 
-  @media ${breakpoints.TABLET_WIDTH} {
+  flex-basis: ${({ c1, theme }) =>
+    `calc(${(c1 / theme.vars.columns.mobile) * 100}%)`};
+  max-width: ${({ c1, theme }) =>
+    `calc(${(c1 / theme.vars.columns.mobile) * 100}%)`};
+  display: ${({ c1 }) => (c1 ? 'block' : 'none')};
+
+  @media ${breakpoints.mobileM} {
     flex-basis: ${({ c2, theme }) =>
       `calc(${(c2 / theme.vars.columns.tablet) * 100}%)`};
     max-width: ${({ c2, theme }) =>
@@ -45,11 +47,11 @@ export const Col = styled.div`
     display: ${({ c2 }) => (c2 ? 'block' : 'none')};
   }
 
-  @media ${breakpoints.mobileM} {
-    flex-basis: ${({ c1, theme }) =>
-      `calc(${(c1 / theme.vars.columns.mobile) * 100}%)`};
-    max-width: ${({ c1, theme }) =>
-      `calc(${(c1 / theme.vars.columns.mobile) * 100}%)`};
-    display: ${({ c1 }) => (c1 ? 'block' : 'none')};
+  @media ${breakpoints.tablet} {
+    flex-basis: ${({ c3, theme }) =>
+      `calc(${(c3 / theme.vars.columns.desktop) * 100}%)`};
+    max-width: ${({ c3, theme }) =>
+      `calc(${(c3 / theme.vars.columns.desktop) * 100}%)`};
+    display: ${({ c3 }) => (c3 ? 'block' : 'none')};
   }
 `;
