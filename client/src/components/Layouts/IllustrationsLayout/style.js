094008f9cfa0ff6e2dae4f6f7496fdcae3a44bf9
RamyAlshurafa <34629478+RamyAlshurafa@users.noreply.github.com>
2020-07-28 14:38:15 +0300

    655 create illustrations (#668)
    * create layout skeleton
    
    * Illustratinos layout

---
+++ client/src/components/Layouts/IllustrationsLayout/style.js
@@ -0,0 +1,65 @@
+import styled from 'styled-components';
+import { breakpoints } from '../../../theme';
+
+export const Wrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+
+  @media ${breakpoints.tablet} {
+    flex-direction: row;
+  }
+`;
+
+export const ColouredSideDiv = styled.div`
+  width: 50%;
+  background: ${({ theme, color = 'pink' }) => theme.colors[color]};
+  display: flex;
+  align-items: flex-end;
+  justify-content: flex-end;
+  position: fixed;
+  min-height: 100vh;
+  right: 0;
+`;
+
+export const ContentWrapper = styled.div`
+  width: 100%;
+
+  padding: ${() => `0 3% 0`};
+
+  @media ${breakpoints.mobileM} {
+    padding: ${() => `0 7% 0`};
+  }
+
+  @media ${breakpoints.tablet} {
+    padding: ${() => `0 4% 0`};
+    width: 50%;
+  }
+
+  @media ${breakpoints.laptop} {
+    padding: ${() => `0 5% 0`};
+  }
+
+  @media ${breakpoints.laptopL} {
+    padding: ${() => `0 7% 0`}; /** 7% equal 100px on 1440px screens */
+  }
+`;
+
+export const ColouredTopDiv = styled.div`
+  background: ${({ theme, color = 'pink' }) => theme.colors[color]};
+  min-height: 140px;
+  height: 30vw;
+  overflow: hidden;
+  display: flex;
+  justify-content: flex-end;
+  padding-right: 10%;
+`;
+
+export const Image = styled.img`
+  max-width: 600px;
+  width: 80%;
+`;
+
+export const TopImage = styled.img`
+  max-width: 100%;
+  height: 150%;
+`;
