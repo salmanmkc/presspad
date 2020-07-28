28932b7e10610c1a4b4b2059adee3f57f5547881
fadeomar <fadeomar2015@gmail.com>
2020-07-28 12:49:23 +0300

    add story to the title component

---
+++ client/src/stories/9-Title.stories.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Title from '../components/Common/Title';
+
+export default {
+  title: 'Title',
+};
+
+export const TitleCompoent = () => (
+  <div style={{ width: 600 }}>
+    <Title>Welcome to the PressPad Movement!</Title>
+  </div>
+);
