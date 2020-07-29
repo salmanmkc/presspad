eb8770a5150651518797092b5733106add77907a
Fadi Omar <39485495+fadeomar@users.noreply.github.com>
2020-07-29 10:16:30 +0300

    640 update typograpy (#665)
    * update heading4 font size
    
    * add heading5 to typography
    
    * add heading 0 to typography and update h7caps
    
    * update normal body font size
    
    * update font size for link
    
    * add Glacial Indifference font family to the app
    
    * update the line height for typography components
    
    * remove ArmWrestler font face
    
    * fix web font url path
    
    * fix web font url path

--- client/src/stories/3-Typography.stories.js
+++ client/src/stories/3-Typography.stories.js
@@ -5,6 +5,8 @@ import {
   H2,
   H3,
   H4,
+  H5,
+  H0C,
   H1C,
   H2C,
   H3C,
@@ -33,13 +35,15 @@ export const Heading1 = () => <H1>Heading 1 (H1)</H1>;
 export const Heading2 = () => <H2>Heading 2 (H2)</H2>;
 export const Heading3 = () => <H3>Heading 3 (H3)</H3>;
 export const Heading4 = () => <H4>Heading 4 (H4)</H4>;
-export const Heading1Caps = () => <H1C>Heading 1 Caps (H1C)</H1C>;
-export const Heading2Caps = () => <H2C>Heading 2 Caps (H2C)</H2C>;
-export const Heading3Caps = () => <H3C>Heading 3 Caps (H3C)</H3C>;
-export const Heading4Caps = () => <H4C>Heading 4 Caps (H4C)</H4C>;
-export const Heading5Caps = () => <H5C>Heading 5 Caps (H5C)</H5C>;
-export const Heading6Caps = () => <H6C>Heading 6 Caps (H6C)</H6C>;
-export const Heading7Caps = () => <H7C>Heading 7 Caps (H7C)</H7C>;
+export const Heading5 = () => <H5>Heading 5 (H5)</H5>;
+export const Heading0Caps = () => <H0C>Heading 0 Caps(130)</H0C>;
+export const Heading1Caps = () => <H1C>Heading 1 Caps (H1C)(60)</H1C>;
+export const Heading2Caps = () => <H2C>Heading 2 Caps (H2C)(40)</H2C>;
+export const Heading3Caps = () => <H3C>Heading 3 Caps (H3C)(30)</H3C>;
+export const Heading4Caps = () => <H4C>Heading 4 Caps (H4C)(24)</H4C>;
+export const Heading5Caps = () => <H5C>Heading 5 Caps (H5C)(20)</H5C>;
+export const Heading6Caps = () => <H6C>Heading 6 Caps (H6C)(18)</H6C>;
+export const Heading7Caps = () => <H7C>Heading 7 Caps (H7C)(14)</H7C>;
 
 const loremIpsum = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna`;
 
@@ -48,6 +52,13 @@ export const PExtraLarge = () => (
     Extra Large body text (PXL) <br />
     {loremIpsum}
   </PXL>
+);
+
+export const PLargeBold = () => (
+  <PL bold>
+    Large body text (PL) <br />
+    {loremIpsum}
+  </PL>
 );
 
 export const PLarge = () => (
