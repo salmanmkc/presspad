01bb280cb8a1edaef0f6339e64a9f6e634f509b6
fadeomar <fadeomar2015@gmail.com>
2020-07-29 11:33:06 +0300

    update the figure with h5 typo

--- client/src/components/Common/Figure/index.js
+++ client/src/components/Common/Figure/index.js
@@ -9,8 +9,7 @@ const Container = styled.div`
 const Figure = withTheme(({ stats, title, statsColor, titleColor }) => (
   <Container>
     <T.H1 color={statsColor || 'pink'}>{stats}</T.H1>
-    {/* this should be H5 */}
-    <T.H4 color={titleColor || 'blue'}>{title}</T.H4>
+    <T.H5 color={titleColor || 'blue'}>{title}</T.H5>
   </Container>
 ));
 
