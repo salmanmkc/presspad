3d6bc5afd134cea144a86136f7eb2b65445ae316
fadeomar <fadeomar2015@gmail.com>
2020-07-30 12:43:35 +0300

    fix vertical spacing in figure component

--- client/src/components/Common/Figure/index.js
+++ client/src/components/Common/Figure/index.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import styled, { withTheme } from 'styled-components';
 import * as T from '../Typography';
 
-const Container = styled.div`
-  width: 100%;
-`;
-
-const Figure = withTheme(({ stats, title, statsColor, titleColor }) => (
-  <Container>
-    <T.H1 color={statsColor || 'pink'}>{stats}</T.H1>
-    <T.H5 color={titleColor || 'blue'}>{title}</T.H5>
-  </Container>
-));
+const Figure = ({ stats, title, statsColor, titleColor }) => (
+  <div>
+    <T.H1
+      color={statsColor || 'pink'}
+      style={{ textAlign: 'center', lineHeight: 1 }}
+    >
+      {stats}
+    </T.H1>
+    <T.H5 color={titleColor || 'blue'} style={{ textAlign: 'center' }}>
+      {title}
+    </T.H5>
+  </div>
+);
 
 export default Figure;
