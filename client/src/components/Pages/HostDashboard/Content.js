7d8c87f1c1d53dc3c9d58aa09051a937bb69dbb8
Simon Dupree <dupreenator@gmail.com>
2020-07-30 19:13:43 +0200

    added titles and sectiontitles

--- client/src/components/Pages/HostDashboard/Content.js
+++ client/src/components/Pages/HostDashboard/Content.js
@@ -19,36 +19,63 @@ const Content = ({ name, windowWidth }) => {
     },
   };
 
+  const typographies = {
+    headerTitle: {
+      desktop: T.H2,
+      mobile: T.H3,
+    },
+    sectionTitle: {
+      desktop: T.H4C,
+      mobile: T.H6C,
+    },
+  };
+  const HeaderTitle = typographies.headerTitle[device];
+  const SectionTitle = typographies.sectionTitle[device];
+
   return (
     <>
       {/* HEADER */}
       <Row mb={5}>
         <Col w={[4, 12, 12]}>
-          <T.H2>Welcome back, {firstName}</T.H2>
+          <HeaderTitle>Welcome back, {firstName}</HeaderTitle>
         </Col>
       </Row>
       {/* NEXT BOOKING / WALLET */}
       <Row mb={bottomMargins.row[device]}>
         <Col w={[4, 7, 8]} mb={bottomMargins.col[device]}>
-          <div style={{ border: '1px solid' }}>Upcoming Booking</div>
+          <div style={{ border: '1px solid' }}>
+            <SectionTitle>Upcoming Booking</SectionTitle>
+          </div>
         </Col>
         <Col w={[4, 5, 4]} mb={bottomMargins.col[device]}>
-          <div style={{ border: '1px solid' }}>My Wallet</div>
+          <div style={{ border: '1px solid' }}>
+            {' '}
+            <SectionTitle>My Wallet</SectionTitle>
+          </div>
         </Col>
       </Row>
       {/* BOOKING DATES / UPDATES */}
       <Row mb={bottomMargins.row[device]}>
         <Col w={[4, 7, 8]} mb={bottomMargins.col[device]}>
-          <div style={{ border: '1px solid' }}>Booking Details</div>
+          <div style={{ border: '1px solid' }}>
+            {' '}
+            <SectionTitle>Booking Details</SectionTitle>
+          </div>
         </Col>
         <Col w={[4, 5, 4]} mb={bottomMargins.col[device]}>
-          <div style={{ border: '1px solid' }}>Updates</div>
+          <div style={{ border: '1px solid' }}>
+            {' '}
+            <SectionTitle>Updates</SectionTitle>
+          </div>
         </Col>
       </Row>
       {/* REVIEWS / PAYMENTS / SOCIAL */}
       <Row mb={bottomMargins.row[device]}>
         <Col w={[4, 7, 8]} mb={bottomMargins.col[device]}>
-          <div style={{ border: '1px solid' }}>Reviews</div>
+          <div style={{ border: '1px solid' }}>
+            {' '}
+            <SectionTitle>Recent Reviews</SectionTitle>
+          </div>
         </Col>
         <Col w={[4, 5, 4]} mb={bottomMargins.col[device]}>
           <div
@@ -64,9 +91,12 @@ const Content = ({ name, windowWidth }) => {
                 marginBottom: '10px',
               }}
             >
-              Payments
+              <SectionTitle>Recent Payments</SectionTitle>
             </div>
-            <div style={{ border: '1px solid' }}>Social</div>
+            <div style={{ border: '1px solid' }}>
+              {' '}
+              <SectionTitle>Join the Presspad Community</SectionTitle>
+            </div>
           </div>
         </Col>
       </Row>
