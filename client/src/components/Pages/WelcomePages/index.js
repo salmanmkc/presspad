5de0902258d704584d81ab643a56b1d30064332b
fadeomar <fadeomar2015@gmail.com>
2020-08-01 19:33:00 +0300

    add the content of the inern user welcome pages

---
+++ client/src/components/Pages/WelcomePages/index.js
@@ -0,0 +1,56 @@
+import React from 'react';
+import Welcome from './Welcome';
+
+const internData = [
+  {
+    title: 'welcome to the presspad movement!',
+    subTitle: 'That’s it! You’ve successfully signed up.',
+    content:
+      'As soon as our team has approved your account you’ll be ready to book your first host and prepare for your stay! It’s an exciting and maybe nerve-wracking time but don’t worry, we’ve got your back. And not just when it comes to finding a place to stay, we’ve got tonnes of advice and support available too.',
+  },
+  {
+    title: 'YOU’VE GOT MAIL!',
+    subTitle: 'A welcome email is winging its way to your inbox...',
+    content:
+      'But we promise we won’t make a habit of it! We’ll only message you about the really important stuff - when you get responses to your booking requests or when we need something from you to proceed with a booking. Make sure to check your spam and mark emails from PressPad as not spam so you don’t miss these!',
+  },
+  {
+    title: 'FIND YOUR FIRST HOST AND HOMESTAY',
+    subTitle:
+      'Once you’re approved, you can find a host and a new home for your placement.',
+    content:
+      'You can filter hosts when you search based on location, dates and interests, view their profiles and when you’re ready, send them a booking request. We’ll let you know how much of a discount you might be eligible for through our - and our partner organisations’ - bursary and discount programmes!',
+  },
+  {
+    title: 'MAKING PAYMENT EASY',
+    subTitle:
+      'Once you’ve had your booking request accepted by your host you’ll need to pay within 48 hours to secure your booking.',
+    content:
+      'You can choose to pay upfront or in installments for longer stays. And remember, right now the first 2 weeks are on us - that means completely free for you! If you’ve got a discount code or a bursary - just follow the instructions at the checkout.',
+  },
+  {
+    title: 'SHAPE OUR COMMUNITY',
+    subTitle:
+      'PressPad isn’t just about accommodation, it’s about connecting like-minded people at every stage of their career.',
+    content:
+      'That’s why we’ve got tonnes of events, freebies, activities and resources just for you. Get involved online to shape our movement - as with anything: you get what you give!',
+  },
+  {
+    title: 'FINALLY... THANK YOU!',
+    subTitle: 'We can only do what we do thanks to your trust and talent!',
+    content:
+      'YOU are PressPad and we are so grateful you’ve decided to be part of a family that cares about the future of our media, and who gets to tell our world’s stories.  YOU are the next generation of the media - and helping you successed makes everything we do worthwhile!',
+  },
+];
+
+const WelcomePages = () => (
+  <div>
+    <Welcome
+      title={internData[0].title}
+      subTitle={internData[0].subTitle}
+      content={internData[0].content}
+    />
+  </div>
+);
+
+export default WelcomePages;
