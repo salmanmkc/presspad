2c6e2881be6dafca91aa92eb8b28ca4bd4fb17a2
fadeomar <fadeomar2015@gmail.com>
2020-08-01 19:33:57 +0300

    create progress component

---
+++ client/src/components/Common/ProgressBar/index.js
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { withTheme } from 'styled-components';
+import * as S from './styled';
+import Button from '../ButtonNew';
+import { Col, Row } from '../Grid';
+
+const ProgressBar = withTheme(
+  ({
+    theme,
+    number,
+    current: initialCurrent,
+    color,
+    handleClick,
+    margin,
+    endFunc,
+  }) => {
+    const [current, setCurrent] = useState(initialCurrent || 0);
+    if (current === number) {
+      // should direct the user to the correct path after get start section
+      if (endFunc && typeof endFunc === 'function') {
+        endFunc();
+      } else {
+        setCurrent(0);
+      }
+    }
+    return (
+      <Row>
+        <Col w={[4, 6, 6]}>
+          <S.Wrapper margin={margin}>
+            {Array.from({ length: number }, (v, i) => (
+              <S.Circle
+                theme={theme}
+                index={i}
+                current={current}
+                color={color}
+                onClick={event => handleClick(event, i)}
+                clickable={handleClick}
+              />
+            ))}
+          </S.Wrapper>
+        </Col>
+        <Col w={[4, 4, 4]}>
+          <Button type="secondary" onClick={() => setCurrent(old => old + 1)}>
+            asd
+          </Button>
+        </Col>
+      </Row>
+    );
+  },
+);
+
+export default ProgressBar;
