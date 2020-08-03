5d3627f12eefff99c133e98fb665b2d598abda67
fadeomar <fadeomar2015@gmail.com>
2020-08-03 09:47:41 +0300

    add the components to the welcome  page

--- client/src/components/Pages/WelcomePages/Welcome.js
+++ client/src/components/Pages/WelcomePages/Welcome.js
@@ -1,15 +1,40 @@
 import React from 'react';
-
+import { withTheme } from 'styled-components';
+import Title from '../../Common/Title';
 import ProgressBar from '../../Common/ProgressBar';
+import { Col, Row } from '../../Common/Grid';
+import * as S from './styled';
+import * as T from '../../Common/Typography';
 
-const Welcome = ({ title, subTitle, contnet, number, current }) => {
+const Welcome = ({
+  title,
+  subTitle,
+  content,
+  number,
+  current,
+  handleClick,
+}) => {
   console.log('get started');
 
   return (
-    <div>
-      <ProgressBar number={4} current={1} color="pink" />
+    <div style={{ display: 'flex', flexDirection: 'column' }}>
+      <S.TitleContainer>
+        <Title>{title}</Title>
+      </S.TitleContainer>
+      <S.SubTitleContainer>
+        <T.H5 color="pink"> {subTitle}</T.H5>
+      </S.SubTitleContainer>
+      <T.P>{content}</T.P>
+      <div style={{ marginTop: 100 }}>
+        <ProgressBar
+          number={number}
+          current={current}
+          color="pink"
+          handleClick={handleClick}
+        />
+      </div>
     </div>
   );
 };
 
-export default Welcome;
+export default withTheme(Welcome);
