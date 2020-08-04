8cf59906cb5f91b70fd4c0b7ef3454e515e7c271
fadeomar <fadeomar2015@gmail.com>
2020-08-04 10:26:21 +0300

    add min height to the title component

--- client/src/components/Pages/WelcomePages/styled.js
+++ client/src/components/Pages/WelcomePages/styled.js
@@ -13,45 +13,16 @@ export const TitleContainer = styled.div`
 `;
 
 export const TitleWrapper = styled.div`
-  margin-top: 0px;
-  min-height: auto;
-  @media ${({ theme }) => theme.breakpoints.tablet} {
-    margin-top: 50px;
-    min-height: 80px;
-  }
-
-  @media (max-width: 1150px) {
-    margin-top: 100px;
-  }
-
-  @media (max-width: 1250px) {
-    margin-top: 100px;
-  }
-
-  @media (max-width: 1100px) {
-    margin-top: 120px;
-  }
-
-  @media (max-width: 1040px) {
-    margin-top: 140px;
-  }
-
-  @media (max-width: 985px) {
-    margin-top: 160px;
-  }
-
-  @media (max-width: 950px) {
-    margin-top: 20px;
-  }
+  min-height: 200px;
 `;
 
 export const SubTitleContainer = styled.div`
   margin-top: -50px;
   @media ${({ theme }) => theme.breakpoints.mobileXL} {
-    margin-top: -120px;
+    margin-top: -50px;
   }
   @media ${({ theme }) => theme.breakpoints.tablet} {
-    margin-top: 0px;
+    margin-top: 50px;
   }
 `;
 
