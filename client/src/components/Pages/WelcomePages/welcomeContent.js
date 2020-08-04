44d4098e51bd74306a9dfa7b6568ede300f5ed55
fadeomar <fadeomar2015@gmail.com>
2020-08-04 10:25:09 +0300

    detrmine the top and bottom titles

--- client/src/components/Pages/WelcomePages/welcomeContent.js
+++ client/src/components/Pages/WelcomePages/welcomeContent.js
@@ -20,6 +20,8 @@ export const internData = [
   },
   {
     title: 'MAKING PAYMENT EASY',
+    topTitle: 'MAKING',
+    bottomTitle: 'PAYMENT EASY',
     subTitle:
       'Once you’ve had your booking request accepted by your host you’ll need to pay within 48 hours to secure your booking.',
     content:
@@ -34,10 +36,78 @@ export const internData = [
   },
   {
     title: 'FINALLY... THANK YOU!',
+    topTitle: 'FINALLY...',
+    bottomTitle: 'THANK YOU!',
     subTitle: 'We can only do what we do thanks to your trust and talent!',
     content:
       'YOU are PressPad and we are so grateful you’ve decided to be part of a family that cares about the future of our media, and who gets to tell our world’s stories.  YOU are the next generation of the media - and helping you successed makes everything we do worthwhile!',
   },
 ];
 
-export const data = [];
+export const hostData = [
+  {
+    title: 'Welcome to the PressPad Movement!',
+    subTitle: 'We can’t do what we do without our hosts. Quite literally.',
+    content:
+      'So we’re thrilled you’ve decided to join us on our mission to improve access to the media. You’ve successfully signed up as a host and now all that’s left is for one of our friendly PressPad team to review your profile and schedule a property check.',
+  },
+  {
+    title: 'YOU’VE GOT MAIL!',
+    subTitle: 'We’ve just sent you a welcome email...',
+    content:
+      'But we promise we won’t make a habit of it! We’ll only message you about the really important stuff - when you get booking requests or we need something from you to proceed with a booking request. Make sure to check your spam and mark emails from PressPad as not spam so you don’t miss these!',
+  },
+  {
+    title: 'COMPLETE CONTROL OF YOUR LISTING',
+    subTitle:
+      'You can update your profile at any time, including putting your account on pause.',
+    content:
+      'If you’re super keen and have lots of availability you can opt to accept bookings automatically. When you accept a booking you’ll have everything you need on your dashboard at your fingertips, from information on your next intern guest to our best hosting tips and how to get in touch with a member of our team.',
+  },
+  {
+    title: 'SHAPE OUR COMMUNITY',
+    subTitle:
+      'PressPad isn’t just about accommodation, it’s about connecting like-minded people at every stage of their career.',
+    content:
+      'That’s why we’ve got tonnes of events, freebies, activities and resources just for you. Get involved online to shape our movement - as with anything: you get what you give!',
+  },
+  {
+    title: 'FINALLY... THANK YOU!',
+    topTitle: 'FINALLY...',
+    bottomTitle: 'THANK YOU!',
+    subTitle:
+      'We’ve said it before but it’s worth repeating: we can’t do what we do without you.',
+    content:
+      'YOU are PressPad and we are so grateful you’ve decided to be part of a family that cares about the future of our media, and who gets to tell our world’s stories. PressPad is about making a difference by doing something different, and we’re thrilled you’re joining us in our mission for change!',
+  },
+];
+
+export const orgData = [
+  {
+    title: 'Welcome to the PressPad Movement!',
+    content:
+      'That’s it! You’ve successfully signed up. As soon as our team has approved your account you’ll be ready to create your first set of discount codes and help your talented new employees afford the opportunities that are going to advance their career and your organisation. We know that improving diversity, and making sure there are equal opportunities into and within your organisation is tough. That’s why we’re here to help and make your job easier - and we want PressPad and this platform to grow with your ambitions to become the best place for anyone and everyone to do excellent journalism: This really is just the beginning!',
+  },
+  {
+    title: 'PLEASE CHECK YOUR EMAIL',
+    content:
+      'A welcome email is winging its way to your inbox...but we promise we won’t make a habit of it! We’ll only message you about the really important stuff - when an intern or an employee with a discount code books in with a host or if we need to verify an intern’s placement is going ahead. Make sure to check your spam and mark emails from PressPad as not spam so you don’t miss these!',
+  },
+  {
+    title: 'REVIEW YOUR PROFILE',
+    content:
+      'You can update and review your profile. We’ll always make sure you can see which of your interns or employees are using PressPad, who is using discount codes and the details of their hosting. We believe this is important as while no one using PressPad is under 18, we understand you care about your employees wellbeing. This is why we have the most rigorous safeguarding checks for hosts, watertight insurance for everyone using the platform - including your organisation and we are always on hand to help with any queries or emergencies.',
+  },
+  {
+    title: 'SHAPE THE PRESSPAD COMMUNITY',
+    content:
+      'PressPad isn’t just about accommodation, it’s about connecting like-minded people at every stage of their career. That’s why we’ve got tonnes of events, freebies, activities and resources for our hosts and interns. Got an event, work opportunity, or resource you want to get out there to our community of motivated, diverse and talented young people? Post it on your profile and we’ll make sure all our members see it!',
+  },
+  {
+    title: 'FINALLY, THANK YOU',
+    topTitle: 'FINALLY,',
+    bottomTitle: 'THANK YOU!',
+    content:
+      'We can only do what we do thanks to your trust and financial support! We believe passionately that a diverse newsroom and organisation solves so many editorial and business challenges so you’re not just doing the right thing, but the smart thing - for your colleagues, audiences, output and the future of your company. YOU are PressPad and we are so grateful you’ve decided to be part of a family that cares about the future of our media, and who gets to tell the world’s stories.',
+  },
+];
