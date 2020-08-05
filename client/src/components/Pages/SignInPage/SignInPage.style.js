827d86d0784f5d1581c31d277228676a7293d8e3
fadeomar <fadeomar2015@gmail.com>
2020-08-05 12:34:50 +0300

    add style for the sign in page components

--- client/src/components/Pages/SignInPage/SignInPage.style.js
+++ client/src/components/Pages/SignInPage/SignInPage.style.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Link } from 'react-router-dom';
 
 import { colors, shadows } from '../../../theme';
 
@@ -50,4 +51,31 @@ export const Disclaimer = styled.div``;
 
 export const ErrorMsg = styled.div`
   color: ${colors.red};
+`;
+
+export const ForgetLink = styled(Link)`
+  color: ${({ theme }) => theme.colors.gray3};
+`;
+
+export const TitleContainer = styled.div`
+  position: absolute;
+  left: 0;
+  top: 40px;
+  width: 500px;
+`;
+
+export const TitleWrapper = styled.div`
+  width: auto;
+  height: 110px;
+  display: none;
+  @media ${({ theme }) => theme.breakpoints.tablet} {
+    display: block;
+  }
+`;
+
+export const TabletTitle = styled.div`
+  display: block;
+  @media ${({ theme }) => theme.breakpoints.tablet} {
+    display: none;
+  }
 `;
