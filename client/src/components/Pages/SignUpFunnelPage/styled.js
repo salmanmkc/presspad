75a56a010bef4f7a3b49a56518abae37339eeccf
fadeomar <fadeomar2015@gmail.com>
2020-08-05 17:04:08 +0300

    creat styled component for the funnel sign up page

---
+++ client/src/components/Pages/SignUpFunnelPage/styled.js
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  min-height: 300px;
+  width: 100%;
+  @media ${({ theme }) => theme.breakpoints.tablet} {
+    width: 80%;
+    min-height: 420px;
+  }
+  @media ${({ theme }) => theme.breakpoints.laptop} {
+    min-height: 450px;
+  }
+`;
+export const Wrapper = styled.div`
+  padding-left: 30px;
+  @media ${({ theme }) => theme.breakpoints.tablet} {
+    padding-left: 120px;
+  }
+`;
