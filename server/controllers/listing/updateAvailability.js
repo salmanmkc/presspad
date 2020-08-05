ae3d56232165d012666694ee457afb748bd22bc5
Simon Dupree <dupreenator@gmail.com>
2020-08-05 12:09:48 +0200

    updated controller

--- server/controllers/listing/updateAvailability.js
+++ server/controllers/listing/updateAvailability.js
@@ -2,23 +2,38 @@ const boom = require('boom');
 const {
   updateListingAvailableDates,
 } = require('../../database/queries/listings');
+const {
+  updateHostAcceptAutomatically,
+} = require('../../database/queries/user');
+
+const { validate } = require('../../middlewares/validation/index');
 
 const updateHostAvailability = async (req, res, next) => {
   //   const currentUser = req.user.id;
   try {
     const { user, body } = req;
-    const { dates, acceptBookingsAutomatically } = req.body;
+    const { _id, role } = user;
+    const { availableDates, acceptAutomatically } = body;
 
-    if (user.role !== 'host') {
+    if (role !== 'host') {
       return next(boom.forbidden());
     }
+    if (!availableDates || !acceptAutomatically) {
+      return next(boom.badData());
+    }
+    const validRequest = await validate({
+      ...availableDates,
+      acceptAutomatically,
+    });
+    if (validRequest) {
+      const [updatedListing, updatedHostAcceptBookings] = await Promise.all([
+        updateListingAvailableDates(_id, availableDates),
+        updateHostAcceptAutomatically(acceptAutomatically),
+      ]);
 
-    const [updatedListing, updatedHostAcceptBookings] = await Promise.all([
-      updateListingAvailableDates(user._id, dates),
-      updateHostAcceptBookings(acceptBookingsAutomatically),
-    ]);
-
-    return res.json({ updatedListing, updatedHostAcceptBookings });
+      return res.json({ updatedListing, updatedHostAcceptBookings });
+    }
+    return next(boom.badData());
   } catch (err) {
     return next(boom.badImplementation(err));
   }
