99313dbc778ced6a0b6385725f3b4989bc950f5b
fadeomar <fadeomar2015@gmail.com>
2020-08-06 16:22:14 +0300

    change the fonts sizes to 18px

--- client/src/components/Pages/SignUpFunnelPage/styled.js
+++ client/src/components/Pages/SignUpFunnelPage/styled.js
@@ -3,11 +3,25 @@ import styled from 'styled-components';
 export const Container = styled.div`
   min-height: 300px;
   width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
   @media ${({ theme }) => theme.breakpoints.tablet} {
     width: 80%;
-    min-height: 420px;
+    min-height: 470px;
   }
   @media ${({ theme }) => theme.breakpoints.laptop} {
+    min-height: 430px;
+  }
+
+  @media (min-width: 1150px) {
+    min-height: 380px;
+  }
+  @media (min-width: 1275px) {
+    min-height: 360px;
+  }
+  @media (min-width: 1350) {
     min-height: 450px;
   }
 `;
