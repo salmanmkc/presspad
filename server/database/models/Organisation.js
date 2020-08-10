d652bd183d9f81cd7ea820ac5640f13683501997
Simon Dupree <dupreenator@gmail.com>
2020-08-10 13:05:50 +0200

    added new fields and updated dummy data

--- server/database/models/Organisation.js
+++ server/database/models/Organisation.js
@@ -9,6 +9,62 @@ const organisationSchema = new Schema(
       required: true,
       trim: true,
     },
+    description: {
+      type: String,
+      required: true,
+    },
+    accountDetails: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+    },
+    contactDetails: {
+      firstName: {
+        type: String,
+        required: true,
+      },
+      lastName: {
+        type: String,
+        required: true,
+      },
+      phone: {
+        type: String,
+        required: true,
+      },
+      email: {
+        type: String,
+        required: true,
+      },
+    },
+    internshipOpportunities: [
+      {
+        opportunity: {
+          type: String,
+          required: true,
+        },
+        link: {
+          type: String,
+          required: true,
+        },
+        details: {
+          type: String,
+          required: true,
+        },
+      },
+    ],
     account: {
       type: mongoose.Types.ObjectId,
       ref: 'accounts',
