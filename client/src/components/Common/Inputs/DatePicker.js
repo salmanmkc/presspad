fc8095fcf5aabdc7c0fbc503f6e492afbdb2d5c9
fadeomar <fadeomar2015@gmail.com>
2020-08-13 14:34:01 +0300

    pass error message to the datePicker component

--- client/src/components/Common/Inputs/DatePicker.js
+++ client/src/components/Common/Inputs/DatePicker.js
@@ -67,8 +67,14 @@ const DatePicker = ({
               onChange={e => onChange(e, 'startDate', index)}
               value={value && value.startDate}
               disabledDate={current => disabledDate(current, 'start', value)}
+              style={{ position: 'relative' }}
               {...props}
             />
+            {error && error.startDate && (
+              <S.Error style={{ position: 'absolute', left: 0, bottom: -20 }}>
+                {error.startDate}
+              </S.Error>
+            )}
           </>
           <T.H7C color="pink" mr={4} ml={4}>
             until
@@ -86,6 +92,11 @@ const DatePicker = ({
             >
               {children}
             </AntdDatePicker>
+            {error && error.endDate && (
+              <S.Error style={{ position: 'absolute', left: 0, bottom: -20 }}>
+                {error.endDate}
+              </S.Error>
+            )}
           </>
           {multi && (
             <S.CrossBtn
@@ -106,7 +117,7 @@ const DatePicker = ({
             + Add more
           </S.AddBtn>
         )}
-        {error && <S.Error>{error}</S.Error>}
+        {error && typeof error === 'string' && <S.Error>{error}</S.Error>}
         {extraInfo && <InfoSection text={extraInfo} />}
       </>
     );
