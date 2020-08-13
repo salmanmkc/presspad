f6f162951d6ac3df8533a0718c2fc9e5806318c1
fadeomar <fadeomar2015@gmail.com>
2020-08-13 14:45:22 +0300

    add style for the lift section in the under review page

--- client/src/components/Pages/Settings/Intern/UnderReview.js
+++ client/src/components/Pages/Settings/Intern/UnderReview.js
@@ -6,25 +6,28 @@ import { Col, Row } from '../../../Common/Grid';
 
 function UnderReview() {
   return (
-    <div>
-      <Row>
-        <Col w={[4, 12, 12]}>
-          <S.SuccessHeaderWrapper>
-            <T.H2 color="blue">Under review</T.H2>
-          </S.SuccessHeaderWrapper>
-        </Col>
-      </Row>
+    <>
+      <S.Container>
+        <Row>
+          <Col w={[4, 12, 12]}>
+            <S.SuccessHeaderWrapper>
+              <T.H2 color="blue">Under review</T.H2>
+            </S.SuccessHeaderWrapper>
+          </Col>
+        </Row>
 
-      <Row>
-        <Col w={[4, 12, 12]}>
-          <T.P color="gray3" mt={3}>
-            It looks like you’ve updated your DBS information. Our team just
-            need to review these new details before we make your profile public
-            again. This will take no more than a few working days.
-          </T.P>
-        </Col>
-      </Row>
-    </div>
+        <Row>
+          <Col w={[4, 12, 12]}>
+            <T.P color="gray3" mt={3}>
+              It looks like you’ve updated your DBS information. Our team just
+              need to review these new details before we make your profile
+              public again. This will take no more than a few working days.
+            </T.P>
+          </Col>
+        </Row>
+      </S.Container>
+      <S.RightDiv />
+    </>
   );
 }
 
