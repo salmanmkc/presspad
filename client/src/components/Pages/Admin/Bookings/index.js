f7bdf36f89b086c424d866f0f2fe4e34201589e0
thejoefriel <joseph.s.friel@gmail.com>
2020-08-13 12:56:44 +0100

    Remove redundant files

--- client/src/components/Pages/Admin/Bookings/index.js
+++ client/src/components/Pages/Admin/Bookings/index.js
@@ -1,13 +1,10 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 
-import { Modal as AntModal } from 'antd';
 import { Row, Col } from '../../../Common/Grid';
 import Table from '../../../Common/Table';
-import Modal from '../../../Common/Modal';
-import { Input } from '../../../Common/Inputs';
+
 import Notification from '../../../Common/Notification';
-import ButtonNew from '../../../Common/ButtonNew';
 
 import {
   LinkCol,
@@ -25,7 +22,7 @@ import {
 import { ADMIN_USER_DETAILS } from '../../../../constants/navRoutes';
 
 import BookingView from './BookingView/BookingView';
-
+import ApproveRejectModal from './ApproveRejectModal';
 import renderExpandedSection from './renderExpandedSection';
 
 const tabs = ['active', 'history'];
@@ -225,80 +222,15 @@ const AdminBookings = () => {
         open={notificationOpen}
         content={error || 'Changes saved'}
       />
-      {newBookingStatus === 'rejected by admin' && (
-        <Modal
-          visible={modalOpen}
-          title="Rejecting request"
-          onOK={submitAdminReview}
-          onCancel={() => setModalOpen(false)}
-          content={
-            <>
-              <Row>
-                <Col w={[4, 12, 12]}>
-                  <T.PXS mb={2} mt={4}>
-                    Are you sure you want to reject this request?
-                  </T.PXS>
-                  <T.PXS mb={4}>
-                    Please write a message to the intern so they know why you
-                    are rejecting their request
-                  </T.PXS>
-                </Col>
-              </Row>
-              <Row>
-                <Col w={[4, 12, 12]}>
-                  <Input
-                    onChange={handleReject}
-                    textArea
-                    value={rejectMessage}
-                  />
-                </Col>
-              </Row>
-              <Row mb={2} mt={4}>
-                <Col w={[4, 6, 6]}>
-                  <ButtonNew
-                    type="primary"
-                    onClick={submitAdminReview}
-                    loading={loading}
-                  >
-                    Reject booking
-                  </ButtonNew>
-                </Col>
-              </Row>
-            </>
-          }
-        />
-      )}
-      {newBookingStatus === 'pending' && (
-        <Modal
-          visible={modalOpen}
-          onOK={submitAdminReview}
-          onCancel={() => setModalOpen(false)}
-          title="Approving request"
-          hideOkButton
-          content={
-            <>
-              <Row>
-                <Col w={[4, 12, 12]}>
-                  <T.PXS mb={2} mt={4}>
-                    Are you sure you want to approve this request?
-                  </T.PXS>
-                </Col>
-              </Row>
-              <Row mb={2} mt={4}>
-                <Col w={[4, 6, 6]}>
-                  <ButtonNew
-                    type="primary"
-                    onClick={submitAdminReview}
-                    loading={loading}
-                  >
-                    Approve booking
-                  </ButtonNew>
-                </Col>
-              </Row>
-            </>
-          }
-        />
-      )}
+      <ApproveRejectModal
+        newBookingStatus={newBookingStatus}
+        submitAdminReview={submitAdminReview}
+        setModalOpen={setModalOpen}
+        handleReject={handleReject}
+        modalOpen={modalOpen}
+        rejectMessage={rejectMessage}
+        loading={loading}
+      />
     </>
   );
 };
