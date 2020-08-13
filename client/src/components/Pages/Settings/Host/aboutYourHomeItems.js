f21033dc6378979b3717210487611c078cf31b4b
fadeomar <fadeomar2015@gmail.com>
2020-08-13 14:38:05 +0300

    create file for aboutHome list data

---
+++ client/src/components/Pages/Settings/Host/aboutYourHomeItems.js
@@ -0,0 +1,13 @@
+const aboutYourHome = [
+  'Lock on the bathroom door',
+  'Lock on the bedroom door',
+  'Spare key for intern',
+  'Bed linen including towel',
+  'WiFi code made available',
+  'Space provided in fridge and cupboard for intern’s food',
+  'You’ll be there to welcome them on their first day',
+  'You can provide info for someone they can contact if you’re not available (neighbour/ emergency contact/partner etc.) - this will be done once the booking is confirmed',
+  'None of these',
+];
+
+export default aboutYourHome;
